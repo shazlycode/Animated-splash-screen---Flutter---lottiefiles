ef52799bbde0e9fdb9cd671a138f2c767a8ac7fc
shazlycode <dr.ahmedshazly@yahoo.com>
2023-01-10 02:30:29 +0300

    initial

---
+++ android/app/src/main/kotlin/com/shazlycode/sunrise/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shazlycode.sunrise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Lottie.asset('assets/images/sunrise.json', fit: BoxFit.contain),
+        ],
+      )),
+    );
+  }
+}
